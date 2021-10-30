b4f8ddd6d461942a6bf5d39b9f7809f805913f40
Wxste <80433461+Wxste@users.noreply.github.com>
2021-10-30 15:59:08 -0500

    Add files via upload

---
+++ TS_Empty_Clip_Spam (1).lua
@@ -0,0 +1,24 @@
+function remoteSpam()
+    while true do
+        wait(.3)
+            for i = 100,1000 do
+                 --copy and paste because yes
+                game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+                wait(.1)game:GetService("Players").LocalPlayer.Backpack.Glock.Click:FireServer()
+        end
+    end
+end
+coroutine.resume(remoteSpam())
